96f54d7bde20d968477bd4d9279442133aa38f60
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-23 19:04:45 +0200

    Initialize project

---
+++ src/main/java/com/mycompany/spaceflights/SpaceFlightsApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.spaceflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceFlightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceFlightsApplication.class, args);
+    }
+}
